d7b3fa8c48b5d4cb8486312c6b7f755c29047eee
Róbert Oravec <robert.oravec@livesporttv.cz>
2022-04-17 08:31:18 +0200

    feat: Added initial lecture playgrounds

---
+++ Playgrounds/LectureGraphQL.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+import Apollo
+
+// Rick and morty API Playground - https://rickandmortyapi.com/graphql
+
+// TODO: Graphql 1 - Show how does code gen work
+
+// TODO: Graphql 2 - Create Client
+
+// TODO: Graphql 2 - Fetch query
+
+// TODO: Graphql 3 - Play with generated query
---
+++ Playgrounds/LectureREST.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+// Rick and morty API Docs - https://rickandmortyapi.com/documentation
+
+// TODO: REST 1 - Create URL
+
+// TODO: REST 2 - Create URLRequest
+
+// TODO: REST 3 - Update HTTP method (create enum for it)
+
+// TODO: REST 4 - Show how to update headers
+
+// TODO: REST 5 - Show how to update body
+
+// TODO: REST 6 - URLSession call (print data)
+
+// TODO: REST 7 - Handle status code (create enum for it)
+
+// TODO: REST 8 - Create Decodable model
+
+// TODO: REST 9 - Parse model
+
+// TODO: REST 10 - Switch URL with URLComponents and add query parameters
+
+// TODO: REST 11 - Progress of URLSession with async bytes
