ea12717eefed008f001a012f6d83d793136f435d
Róbert Oravec <robert.oravec@livesporttv.cz>
2022-04-19 18:25:36 +0200

    feat: Implemented final solutio

--- Playgrounds/LectureGraphQL.playground/Contents.swift
+++ Playgrounds/LectureGraphQL.playground/Contents.swift
@@ -6,7 +6,41 @@ import Apollo
 // TODO: Graphql 1 - Show how does code gen work
 
 // TODO: Graphql 2 - Create Client
+let normalizedCache = InMemoryNormalizedCache()
+
+let apolloStore = ApolloStore(cache: normalizedCache)
+
+let interceptorProvider = DefaultInterceptorProvider(store: apolloStore)
+
+let endpointURL = URL(string: "https://rickandmortyapi.com/graphql")!
+
+let networkTransport = RequestChainNetworkTransport(
+    interceptorProvider: interceptorProvider,
+    endpointURL: endpointURL
+)
+
+let client = ApolloClient(
+    networkTransport: networkTransport,
+    store: apolloStore
+)
 
 // TODO: Graphql 2 - Fetch query
+
+let query = CharactersQuery()
+
+client.fetch(
+    query: query,
+    cachePolicy: .fetchIgnoringCacheCompletely,
+    queue: .main,
+    resultHandler: { result in
+        print(result)
+        switch result {
+        case let .success(data):
+            print(data)
+        case let .failure(error):
+            print("💔 Error received when performing fetch", error)
+        }
+    }
+)
 
 // TODO: Graphql 3 - Play with generated query
